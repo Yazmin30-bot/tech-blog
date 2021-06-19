fc065ec71328dfc4758d4a3eb176f1b3892f3680
Yazmin Tinoco <ytip30@gmail.com>
2021-06-18 19:43:14 -0500

    Create route handlebar to homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,14 +3,21 @@ const Post = require('../models/Post');
 const withAuth = require('../utils/auth');
 
 
-// route to get all dishes
+
 router.get('/', async (req, res) => {
   try {
-    const postData = await Post.findAll().catch((err) => {
-      res.json(err);
+    // Get all post and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
     });
+    // Serialize data so the template can read it
     const posts = postData.map((post) => post.get({ plain: true }));
-    res.render('all', {
+    res.render('homepage', {
       posts,
       loggedIn: req.session.loggedIn,
     });
